7ba79f25dcaafbf3a3457197a08dfe41c846cae0
maaaarcos25 <marcos.manzano@alumnos.upm.es>
2025-01-30 11:20:07 +0100

    Ejercicio 2

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,8 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+
         Ejercicio1 ejercicio1 = new Ejercicio1();
         ejercicio1.ejercicio1();
+
         Ejercicio2 ejercicio2 = new Ejercicio2();
         ejercicio2.mostrarRanking();
+
+        Ejercicio3 ejercicio3 = new Ejercicio3();
+        ejercicio3.calcularSalario();
     }
 }
